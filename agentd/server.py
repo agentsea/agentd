7aeab53a4f4277b7177b8fc639da3e9bff12b73b
Patrick Barker <patrickbarkerco@gmail.com>
2024-01-18 10:22:46 -0700

    add cors

--- agentd/server.py
+++ agentd/server.py
@@ -6,6 +6,8 @@ import base64
 import uuid
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 import pyautogui
 from mss import mss
 
@@ -32,6 +34,14 @@ from .recording import RecordingSession, lock, sessions
 # from .storage import upload_directory_to_gcs
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
