cc28e3cf3866d5ccf636c18cc06ce6e085e7b52e
Patrick Barker <patrickbarkerco@gmail.com>
2024-01-31 15:23:25 -0700

    init

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (or with sudo). Exiting..."
+   exit 1
+fi
+
+echo "installing base packages..."
+apt update
+apt install -y openssh-server cloud-init
+systemctl start ssh
+systemctl enable ssh
+
 echo "copying services..."
 cp ./conf/agentd.service /etc/systemd/system/agentd.service
 cp ./conf/websockify.service /etc/systemd/system/websockify.service
@@ -20,6 +31,11 @@ systemctl restart x11vnc.service
 systemctl restart xvfb.service
 
 echo "setting up firewall..."
+ufw_status=$(ufw status | grep -o "inactive")
+if [ "$ufw_status" == "inactive" ]; then
+    echo "UFW is inactive. Enabling..."
+    ufw enable
+fi
 # agentd
 # NOTE: currently only allowing SSH tunneling
 # ufw allow 8000/tcp
--- install_deps.sh
+++ install_deps.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "agentsea" ]; then
+    echo "This script must be run as the user 'agentsea'. Exiting..."
+    exit 1
+fi
+
 pip install mss "fastapi[all]" pyautogui pynput
