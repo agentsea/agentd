29bff1436e4167f3f36d06d95a33da50c95ec11c
Patrick Barker <patrickbarkerco@gmail.com>
2024-09-09 22:26:06 -0600

    fix bash script

--- uvicorn-run.sh
+++ uvicorn-run.sh
@@ -2,4 +2,4 @@
 
 cd /config/agentd
 
-exec /config/.local/bin/poetry run uvicorn app:app --host 0.0.0.0 --port 8000 > /config/agentd/agentd.log 2>&1
+exec uvicorn app:app --host 0.0.0.0 --port 8000 > /config/agentd/agentd.log 2>&1
